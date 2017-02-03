622fce30a9be75610827547ea57c6de3efda21bc
J0SH1X <aljoshua.hell@gmail.com>
2017-02-03 01:02:45 +0100

    CM -> AOKP and remove EnhancedNFC

--- aokp.mk
+++ aokp.mk
@@ -4,11 +4,8 @@ $(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Galaxy S6" \
